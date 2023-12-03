5268b74221e49f7ccde54dd1ae968d8a9383753e
accura7e <accura7ee@gmail.com>
2023-12-03 23:50:29 +0300

    add: added method

---
+++ js/index.js
@@ -0,0 +1,27 @@
+
+// // ReactDOM.render(React.createElement('input', {
+// //     placeholder: 'Help text',
+// //     onClick: () => console.log('clicked'),
+// //     onMouseEnter: () => console.log('Mouse Over')
+// // }), document.querySelector('#app'))
+
+// const inputClick = () => console.log('clicked')
+// const mouseOver = () => console.log('MouseOver')
+
+// const elements = <input placeholder="dsklafas" onClick={inputClick} onMouseEnter={mouseOver} />
+// const app =  document.querySelector('#app')
+// ReactDOM.render(elements, app)
+
+
+function toWeirdCase(string){
+  const newArr = Array.from(string)
+  newArr.forEach(function(el){
+    
+      indexOf(el) % 2 === 0 ? el.toUpperCase : el
+      
+  })
+  
+ 
+}
+
+console.log(toWeirdCase("sakjlf"))
